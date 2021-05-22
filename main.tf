2d4103d7a646a6eaf035158c70189930a789c9b5
jayasurya9666 <68177811+jayasurya9666@users.noreply.github.com>
2021-05-22 10:20:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    access_key = "AKIA2QEFLENWCZ57K3KB"
-    secret_key = "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"
+    access_key = "AKIA2O3EDZYGIY6EMTGA"
+    secret_key = "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"
     region = "us-east-1"
 }
 
 data "aws_ami" "my_ami" {
      most_recent      = true
-     #name_regex       = "^mavrick"
-     owners           = ["721834156908"]
+     #name_regex       = "^jaya"
+     owners           = ["719080115724"]
 }
 
 
@@ -16,14 +16,14 @@ resource "aws_instance" "web-1" {
     #ami = "ami-0d857ff0f5fc4e03b"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "LaptopKey"
-    subnet_id = "subnet-01f9c2774277f4015"
-	private_ip = "10.1.1.111"
-    vpc_security_group_ids = ["sg-09c2fc6ae1a93c80e"]
+    key_name = "demo"
+    subnet_id = "subnet-0f2a2e06821332e81"
+	private_ip = "10.10.1.202"
+    vpc_security_group_ids = ["sg-0637fd4886e98ede4"]
     associate_public_ip_address = true	
     tags = {
         Name = "Server-1"
         Env = "Prod"
-        Owner = "Sree"
+        Owner = "jaya"
     }
 }
